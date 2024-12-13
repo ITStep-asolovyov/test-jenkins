6503d893bb38f6aa98ba38c38ca4bc5ead429078
asalauyou <Aliaksandr.Salauyou@alfa-bank.by>
2024-12-13 14:25:52 +0300

    init

--- src/test/java/PositiveTests.java
+++ src/test/java/PositiveTests.java
@@ -3,9 +3,15 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,18 +19,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PositiveTests {
 
     @Test
-    public void test1() {
+    public void test1() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("latest");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
 
-        MutableCapabilities capabilities = new DesiredCapabilities();
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", new HashMap<String,Object>(){
-            {
-                put("enableVNC", true);
-                put("enableVideo",true);
-            }
-        });
-
+        WebDriver driver = new RemoteWebDriver(
+                URI.create("http://localhost:4444/wd/hub").toURL(),
+                capabilities
+        );
 open("www.google.com");    }
 
 }
