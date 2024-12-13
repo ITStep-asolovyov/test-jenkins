1933b38a6b9a8af996ca2656114d9e43568e998a
asalauyou <Aliaksandr.Salauyou@alfa-bank.by>
2024-12-13 13:58:09 +0300

    init

--- src/test/java/PositiveTests.java
+++ src/test/java/PositiveTests.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -7,6 +9,7 @@ public class PositiveTests {
 
     @Test
     public void test1() {
+        Configuration.remote = "http://localhost:8081/wd/hub";
 open("www.google.com");    }
 
 }
