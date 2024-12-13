d95e9228e69ae68ff91100fb16e64ea2b82d4a87
asalauyou <Aliaksandr.Salauyou@alfa-bank.by>
2024-12-13 14:19:38 +0300

    init

--- src/test/java/PositiveTests.java
+++ src/test/java/PositiveTests.java
@@ -1,6 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,7 +14,17 @@ public class PositiveTests {
 
     @Test
     public void test1() {
+
+        MutableCapabilities capabilities = new DesiredCapabilities();
         Configuration.remote = "http://localhost:4444/wd/hub";
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", new HashMap<String,Object>(){
+            {
+                put("enableVNC", true);
+                put("enableVideo",true);
+            }
+        });
+
 open("www.google.com");    }
 
 }
