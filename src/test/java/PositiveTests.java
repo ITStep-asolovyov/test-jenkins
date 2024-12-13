a3f343f53f24ce0959fb161b8f6e6cbadb70e3c5
asalauyou <Aliaksandr.Salauyou@alfa-bank.by>
2024-12-13 13:00:54 +0300

    init

---
+++ src/test/java/PositiveTests.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    public void test1() {
+       assertTrue(true);
+    }
+    @Test
+    public void test2() {
+        assertTrue(true);
+    }
+    @Test
+    public void test3() {
+        assertTrue(true);
+    }
+    @Test
+    public void test4() {
+        assertTrue(true);
+    }
+    @Test
+    public void test5() {
+        assertTrue(true);
+    }
+    @Test
+    public void test6() {
+        assertTrue(true);
+    }
+    @Test
+    public void test7() {
+        assertTrue(true);
+    }
+    @Test
+    public void test8() {
+        assertTrue(true);
+    }
+    @Test
+    public void test9() {
+        assertTrue(true);
+    }
+
+}
